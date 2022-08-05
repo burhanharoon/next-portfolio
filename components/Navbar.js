8ca869f02093de6f420d6d3353985c332d79bf5a
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-08-05 07:22:22 +0500

    Added Logo to Skills Card & Inserted Links to Your Blog and Contact Me Buttons

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,18 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className='text-darkBlue flex justify-between bg-[#EFF6FF] p-4'>
+        <nav className='text-darkBlue flex justify-between bg-[#EFF6FF] p-4 border-2 border-b-white'>
             <h1 className='name text-2xl'>Burhan Haroon</h1>
             <nav className='hidden sm:flex gap-5 text-lg'>
                 <Link href='/'>Home</Link>
-                <Link href='/'>Blogs</Link>
-                <Link href='/'>Contact Me</Link>
+                <Link href='https://dev.to/burhanharoon'>
+                    <a target="_blank"> Blogs</a>
+                </Link>
+
+
+                <Link href="https://www.linkedin.com/in/burhandev" target="_blank">
+                <a target="_blank">Contact Me</a>
+                </Link>
             </nav>
             <svg className="block sm:hidden h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </nav>
