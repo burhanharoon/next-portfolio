5fbefa81bb4d16ad33c2afdb4aa65d7250e0fa7a
steven <steven@burrows.plus.com>
2022-10-19 21:08:52 +0100

    made the menu button work when in a small screen

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,19 +35,41 @@ const Navbar = () => {
             <a target="_blank">Contact Me</a>
           </Link>
         </nav>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)} ariaLabel='menu open/close' >
         <svg
           className="block sm:hidden h-6 w-6"
           stroke="currentColor"
           fill="none"
           viewBox="0 0 24 24"
         >
+      
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="2"
             d="M4 6h16M4 12h16M4 18h16"
           ></path>
-        </svg>
+        </svg> 
+            </button>
+            { isMenuOpen &&  <div className='absolute top-20 right-4 w-64 bg-white p-4 flex items-center rounded-md'>
+              <div className='w-full'>
+              <h2 className="name text-lg pl-2">Burhan Haroon</h2>
+              <p className='py-2 my-2 pl-2 hover:bg-sky-100 w-full rounded-md'><Link href="/" >Home</Link></p>
+              <p className='py-2 my-2 pl-2 hover:bg-sky-100 w-full rounded-md'>
+                <Link href="https://dev.to/burhanharoon">
+                  <a target="_blank"> Blogs</a>
+                </Link>
+              </p>
+              <p className='py-2 my-2 pl-2 hover:bg-sky-100 w-full rounded-md'>
+                <Link href="https://www.linkedin.com/in/burhandev" target="_blank">
+                  <a target="_blank">Contact Me</a>
+                </Link>
+              </p>
+              </div>
+
+
+
+            </div>}
       </nav>
     );
 }
