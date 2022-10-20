0b4bcc937b261094a8fbd72cfa0e1ecb0e3f661d
Avez <aveznqureshi14@gmail.com>
2022-10-20 18:00:15 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import HamburgerIcon from '../assets/icons/hamburger';
 import CrossIcon from '../assets/icons/cross';
+import Git from '../assets/svg/github-svgrepo-com.svg'
+import Instagram from '../assets/svg/instagram.svg'
+import Linkedin from '../assets/svg/linkedin-svgrepo-com.svg'
+import Dev from '../assets/svg/devto.svg'
 
 const Navbar = () => {
   const [colorChange, setColorChange] = useState(false);
@@ -17,9 +21,9 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`text-darkBlue flex justify-between p-4 sticky top-0 w-full navbar transition-all ease-linear duration-300 ${
-        colorChange ? 'bg-white shadow-lg' : 'bg-[#EFF6FF] shadow-none'
-      }  `}
+      className={`text-darkBlue flex justify-between  p-4 sticky top-0 w-full navbar transition-all ease-linear duration-300 ${
+        colorChange ? 'bg-white shadow-lg' : 'bg-[#EFF6FF] shadow-none' 
+      }  `} style={{alignItems:"center !important"}}
     >
       <div
         className={`
@@ -35,7 +39,7 @@ const Navbar = () => {
             isMenuOpen
               ? 'bg-white sm:bg-transparent flex gap-2 sm:gap-5 flex-col sm:flex-row top-[3rem] sm:top-0 absolute sm:relative m-auto sm:m-0 left-0 right-0 rounded-b-lg p-3 sm:p-0 text-base sm:text-lg'
               : 'hidden sm:flex gap-5 text-lg'
-          } transition-all ease-linear`}
+          } transition-all ease-linear`} style={{alignItems:"center !important"}}
         >
           <Link href='/'>
             <div className='hover:bg-blue-50 p-2 sm:p-0 rounded-md'>Home</div>
@@ -49,13 +53,33 @@ const Navbar = () => {
             Blogs
           </a>
           <a
-            href='https://www.linkedin.com/in/burhandev'
+            href='https://github.com/burhanharoon'
             target='_blank'
             rel='noreferrer'
-            className='hover:bg-blue-50 p-2 sm:p-0 rounded-md'
           >
-            Contact Me
+            <Git className="h-10 w-10" />
           </a>
+          <a
+          href='https://instagram.com/kya.bne.ga.burhan.tera'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Instagram className="h-10 w-10" />
+        </a>
+        <a
+        href='https://www.linkedin.com/in/burhandev'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <Linkedin className="h-10 w-10" />
+      </a>
+        <a
+        href='https://dev.to/burhanharoon'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <Dev className="h-10 w-10" />
+      </a>
         </nav>
         <button
           className='block sm:hidden h-8 w-8 hover:bg-blue-50 p-1 rounded-md'
